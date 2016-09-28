4cfa9cb2f039f02fc18438417cbed1a5adf9d2c8
harmonicPerc <ross.bunker9.my@gmail.com>
2016-09-28 14:43:24 -0600

    Added license header to httpserver

--- httpserver.py
+++ httpserver.py
@@ -2,7 +2,7 @@
 
 # MIT License
 # 
-# Copyright (c) [2016] [Ross Bunker]
+# Copyright (c) 2016 Ross Bunker
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
